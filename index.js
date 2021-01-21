7605f9714e1ad8a87affebd3c114f991f253ca65
xlizzyx19 <124626@student.drenthecollege.nl>
2021-01-21 12:13:46 +0100

    voor gezord dat de kaarten kunnen flippen en wanneer er op geklikt word aan welke naam ze gaan en je kan zien welke bij welke je matched in console bij inspecteren

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const cards = document.querySelectorAll('.memory-card');
 
-
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
 function flipCard() {
-  this classList toggle("flip")
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  }else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    console.log(firstCard.dataset.framework);
+    console.log(secondCard.dataset.framework);
+  }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
