6dafd429f123f462c49f689f50fc3f3d225f0e92
RubenPostema <128754@student.drenthecollege.nl>
2021-01-25 16:12:45 +0100

    ik heb ervoor gezorgd dat je ze niet vaker dan 1x kan klikken, en ik heb er voor gezorgd dat als het gene match is ze weer terug flippen

--- index.js
+++ index.js
@@ -12,10 +12,17 @@ function flipCard() {
   }else {
     hasFlippedCard = false;
     secondCard = this;
-
-    console.log(firstCard.dataset.framework);
-    console.log(secondCard.dataset.framework);
-  }
+}
+    if (firstCard.dataset.framework ===
+      secondCard.dataset.framework) {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+      } else {
+        setTimeout(()=>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+      }, 1500);
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
