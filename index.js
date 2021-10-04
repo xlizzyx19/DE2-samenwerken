9217250c26d173bac72212d5c9e0bc4ba73663d7
xlizzyx19 <124626@student.drenthecollege.nl>
2021-10-04 09:20:58 +0200

    kaarten random gemaakt

--- index.js
+++ index.js
@@ -53,4 +53,12 @@ function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
 }
+
+(function shuffle(){
+ cards.forEach(card => {
+   let randomPos = Math.floor(Math.random() * 12);
+   card.style.order = randomPos;
+ });
+})();
+
 cards.forEach(card => card.addEventListener('click', flipCard));
